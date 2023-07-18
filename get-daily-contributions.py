4a8beb539dbebf510169f657319e8faf8fa21c44
Alexander Alemayhu <alexander@alemayhu.com>
2023-07-18 20:02:07 +0200

    feat(get-daily-contributions): Add pagination to fetch all events

--- get-daily-contributions.py
+++ get-daily-contributions.py
@@ -4,14 +4,25 @@ from collections import defaultdict
 
 def get_daily_contributions(username):
     url = f"https://api.github.com/users/{username}/events/public"
-    response = requests.get(url)
-    data = json.loads(response.text)
-    
     contributions = defaultdict(int)
+    pages = 0
 
-    for event in data:
-        date = event["created_at"][:10]  # Extract the date part from timestamp
-        contributions[date] += 1
+    while url and pages < 10:
+        response = requests.get(url)
+        data = json.loads(response.text)
+
+        for event in data:
+            date = event["created_at"][:10]  # Extract the date part from timestamp
+            contributions[date] += 1
+
+        link_header = response.headers.get('Link')
+        if link_header:
+            links = requests.utils.parse_header_links(link_header.rstrip('>').replace('>,<', ',<'))
+            url = next((link['url'] for link in links if link['rel'] == 'next'), None)
+        else:
+            url = None
+
+        pages += 1
 
     for date, count in contributions.items():
         print(f"On {date}, {username} made {count} contributions.")
